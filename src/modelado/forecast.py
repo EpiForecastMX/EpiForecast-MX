ae62ef178808787f88f3321293c02cb1f9dff469
luisgss10 <luisgss10@hotmail.com>
2026-02-17 16:50:07 -0800

    refactor: Se agrega parametro model_path al inicializador de la clase ForecastModelLoader y ruta default

--- src/modelado/forecast.py
+++ src/modelado/forecast.py
@@ -9,8 +9,6 @@ from src.configuraciones.config_params import conf, logger
 class ForecastModelLoader:
     
     def __init__(self, periodo, model_path=None):
-
-        #self.model_path = conf['data']['model_train']
         self.model_path = Path(model_path) if model_path else Path(conf["data"]["model_train"])
         self.model = None
         self.periodo = periodo
